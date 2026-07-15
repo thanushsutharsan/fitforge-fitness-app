c57b182eb75c5984ec53267d4593733e97db2dd6
thanushsutharsan <thanush23@yahoo.com>
2026-07-15 23:38:08 +0100

    feat: add weightloss goal selector functionality in home.js file.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,10 @@
 const weightLossBtn = document.getElementById("weightLossBtn");
 const athleteBtn = document.getElementById("athleteBtn");
 const result = document.getElementById("result");
+
+weightLossBtn.addEventListener("click", () => {
+    result.innerHTML = `
+        <h3>Weight Loss Programme</h3>
+        <p>We'll recommend calorie-controlled meal plans and fat-loss workouts focusing on the core.</p>
+    `;
+});
