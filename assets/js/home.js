ab1ab5d7d5da11c723b184406dfc189bcf45c79d
thanushsutharsan <thanush23@yahoo.com>
2026-07-15 23:39:48 +0100

    feat: add athlete goal selector functionallity in home.js file.

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,3 +8,10 @@ weightLossBtn.addEventListener("click", () => {
         <p>We'll recommend calorie-controlled meal plans and fat-loss workouts focusing on the core.</p>
     `;
 });
+
+athleteBtn.addEventListener("click", () => {
+    result.innerHTML = `
+        <h3>Athlete Performance Programme</h3>
+        <p>We'll recommend strength training looking at muscles and full body workouts, sport-specific nutrition and performance plans.</p>
+    `;
+});
