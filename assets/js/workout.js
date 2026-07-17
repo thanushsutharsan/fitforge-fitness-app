dd05d4f0f332e24c3ed8f10a179427c0ed4b55a2
thanushsutharsan <thanush23@yahoo.com>
2026-07-17 02:32:23 +0100

    feat: connect stopwatch display elements in workout.js

--- assets/js/workout.js
+++ assets/js/workout.js
@@ -2,3 +2,10 @@ const btnStart = document.querySelector('.st');
 const btnReset = document.querySelector('.rs');
 const btnLap = document.querySelector('.lps');
 const btnStop = document.querySelector('.stp');
+
+const hrsBox = document.querySelector('.hrs');
+const minBox = document.querySelector('.min');
+const secBox = document.querySelector('.sec');
+const msBox = document.querySelector('.ms');
+
+const lapContainer = document.querySelector('.laps');
