beb87e08e16ff28474ba4d5501843d231a4339d6
thanushsutharsan <thanush23@yahoo.com>
2026-07-17 02:38:02 +0100

    feat: add stopwatch start timer functionality in workout.js

--- assets/js/workout.js
+++ assets/js/workout.js
@@ -16,3 +16,29 @@ let ms = 0;
 
 let startTimer = null;
 let running = false;
+
+// start 
+btnStart.addEventListener('click', () => {
+
+  if (startTimer !== null) return; 
+
+  running = true;
+
+  startTimer = setInterval(() => {
+
+    ms++;
+    if (ms === 100) {
+      ms = 0;
+      sec++;
+    }
+    if (sec === 60) {
+      sec = 0;
+      min++;
+    }
+    if (min === 60) {
+      min = 0;
+      hrs++;
+    }
+    updateDisplay();
+  }, 10);
+});
