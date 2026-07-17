c3bdac691ceb76b08e03d956cf3c0f6b14d75274
thanushsutharsan <thanush23@yahoo.com>
2026-07-17 02:31:12 +0100

    feat: add stopwatch DOM element selection in workout.js

---
+++ assets/js/workout.js
@@ -0,0 +1,4 @@
+const btnStart = document.querySelector('.st');
+const btnReset = document.querySelector('.rs');
+const btnLap = document.querySelector('.lps');
+const btnStop = document.querySelector('.stp');
