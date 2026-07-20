05178a00da897780c42525513e680709a74cab03
thanushsutharsan <thanush23@yahoo.com>
2026-07-20 23:14:32 +0100

    feat: retrieve calculator inputs in nutrition.js

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,4 +1,12 @@
 const calculateButton = document.getElementById("calculate-btn");
 calculateButton.addEventListener("click", calculateMacros);
 
-function calculateMacros() {}
+function calculateMacros() {
+    const caloriesInput = document.getElementById("calories");
+    const goalInput = document.getElementById("goal");
+    const results = document.getElementById("results");
+
+    const calories = Number(caloriesInput.value);
+    const goal = goalInput.value;
+
+}
