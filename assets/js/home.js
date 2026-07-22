5c4f4be0c0b6fcf23956ada3f45eb500a66f3173
thanushsutharsan <thanush23@yahoo.com>
2026-07-23 00:03:54 +0100

    fix: indentation errors in home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,24 +1,27 @@
+/*global document, localStorage*/
+
 const weightLossBtn = document.getElementById("weightLossBtn");
 const athleteBtn = document.getElementById("athleteBtn");
 const result = document.getElementById("result");
 
-
 if (weightLossBtn && athleteBtn && result) {
-weightLossBtn.addEventListener("click", () => {
-    result.innerHTML = `
-        <h3>Weight Loss Programme</h3>
-        <p>We'll recommend calorie-controlled meal plans and fat-loss workouts focusing on the core.</p>
-    `;
-    // Save user's choice
-    localStorage.setItem("userGoal", "weightLoss");
-});
+    weightLossBtn.addEventListener("click", function () {
+        result.innerHTML =
+        "<h3>Weight Loss Programme</h3>" +
+        "<p>We'll recommend calorie-controlled meal plans and " +
+        "fat-loss workouts. These will focus on improving " +
+        "core strength.</p>";
 
-athleteBtn.addEventListener("click", () => {
-    result.innerHTML = `
-        <h3>Athlete Performance Programme</h3>
-        <p>We'll recommend strength training looking at muscles and full body workouts, sport-specific nutrition and performance plans.</p>
-    `;
-    // Save user's choice
-    localStorage.setItem("userGoal", "athlete");
-});
+        localStorage.setItem("userGoal", "weightLoss");
+    });
+
+    athleteBtn.addEventListener("click", function () {
+        result.innerHTML =
+        "<h3>Athlete Performance Programme</h3>" +
+        "<p>We'll recommend strength training, full body " +
+        "workouts, sport-specific nutrition and " +
+        "performance plans.</p>";
+
+        localStorage.setItem("userGoal", "athlete");
+    });
 }
