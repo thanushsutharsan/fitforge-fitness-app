700ab48e06161732b28b69dd43f659b4938e1f5e
thanushsutharsan <thanush23@yahoo.com>
2026-07-22 19:57:20 +0100

    improve nutrition.js defensive checks

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,11 +1,15 @@
 const calculateButton = document.getElementById("calculate-btn");
-calculateButton.addEventListener("click", calculateMacros);
+if (calculateButton) {
+    calculateButton.addEventListener("click", calculateMacros);
+}
 
 function calculateMacros() {
     const caloriesInput = document.getElementById("calories");
     const goalInput = document.getElementById("goal");
     const results = document.getElementById("results");
-
+    if(!caloriesInput || !goalInput || !results) {
+        return;
+    }
     const calories = Number(caloriesInput.value);
     const goal = goalInput.value;
     
